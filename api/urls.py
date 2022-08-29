5dd567e07e5f92c7026a248a1ff32cc62421703a
Agustin <porlan.agustin@gmail.com>
2022-08-28 23:30:14 -0300

    migrates

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from sucursales.views import SucursalLists,SucursalDetails
 from prestamos.views import PrestamoPost,PrestamoDetails
-from clientes.views import ClienteLists,ClienteDetails,UserDetail,UserList
-
+from clientes.views import ClienteLists,ClienteDetails
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,8 +11,5 @@ urlpatterns = [
     path('api/sucursales/<int:pk>/',SucursalDetails.as_view()),
     path('api/sucursales/',SucursalLists.as_view()),
     path('api/prestamodelete/<int:pk>',PrestamoDetails.as_view()),
-    path('api/prestamopost/', PrestamoPost.as_view()),
-    path('api/users/<int:pk>/', UserDetail.as_view()),
-    path('api/users/', UserList.as_view()),
-    
+    path('api/prestamopost/', PrestamoPost.as_view()) 
 ]
